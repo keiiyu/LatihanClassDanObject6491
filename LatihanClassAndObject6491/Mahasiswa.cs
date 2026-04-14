6c128cb8ef6e6bc16fcca76b47bfcb7b2c7049db
C.Khoirivayanti <tsukishimaritsukei@gmail.com>
2026-04-14 14:55:01 +0700

    Add files via upload

---
+++ LatihanClassAndObject6491/Mahasiswa.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanClassAndObject6491
+{
+    internal class Mahasiswa
+    {
+        public string Nim { get; set; }
+        public string Nama { get; set; }
+        public float Ipk { get; set; }
+        public void Registrasi()
+        {
+            Console.WriteLine("Nama: {0}", Nama);
+            Console.WriteLine("NIM: {0}", Nim);
+            Console.WriteLine("IPK: {0}", Ipk);
+            Console.WriteLine("Telah melakukan registrasi");
+        }
+        public void IsiKrs()
+        {
+            Console.WriteLine("{0} sedang melakukan proses Krs\n", Nama);
+        }
+    }
+}
---
+++ LatihanClassAndObject6491/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanClassAndObject6491
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Mahasiswa saya = new Mahasiswa();
+            Mahasiswa kamu = new Mahasiswa();
+
+            saya.Nim = "12345";
+            saya.Nama = "Jono";
+            saya.Ipk = 3.95f;
+
+            kamu.Nim = "12346";
+            kamu.Nama = "Paijo";
+            kamu.Ipk = 3.70f;
+
+            saya.Registrasi();
+            saya.IsiKrs();
+
+            kamu.Registrasi();
+            kamu.IsiKrs();
+
+            Console.ReadKey();
+        }
+    }
+}
